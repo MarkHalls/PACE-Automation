b013f33737114eae8578c648c64c46ea17424db2
Mark Halls <firesetterdev@gmail.com>
2018-03-20 16:06:32 -0700

    Fix variable names and broke out block parsing to own function

--- projects/approveByTask.py
+++ projects/approveByTask.py
@@ -1,7 +1,7 @@
-#This program accepts csv files stored
-#in c:\projects\approvebytask.csv in the form of "PTN,Date"
-#Author: Mark Halls
-#Last update: 3/10/16
+# This program accepts csv files stored
+# in c:\projects\approvebytask.csv in the form of "PTN,Date"
+# Author: Mark Halls
+# Last update: 3/10/16
 
 # Required Packages:
 # pip install send2trash
@@ -27,101 +27,87 @@ pyautogui.PAUSE = 0.50
 pyautogui.FAILSAFE = True
 
 
-#click function requires arguments ('fullPathToImage', "Error Identifier")
 def click(fullPathToImage, error):
     try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen(fullPathToImage)))
+        pyautogui.click(pyautogui.center(
+            pyautogui.locateOnScreen(fullPathToImage)))
     except:
         print(error, " not found, trying again")
         click(fullPathToImage, error)
 
 
-def start(taskCode):
-    with open('c:/projects/csv/approvebytask.csv', mode='r') as infile:
+def parseBlocks(path):
+    blockList = []
+    with open(path, mode='r') as infile:
         reader = csv.reader(infile)
-        i = 0
-        promote = []
-        newRow = []
+        rowIndex = 0
+        block = []
         for row in reader:
-            if i % 40 == 0:
-                promote.append(newRow)
-                newRow = row
-                #print("1 newlistpromote") 
+            if rowIndex % 40 == 0:
+                blockList.append(block)
+                block = row
             else:
-                newRow = newRow + row
-                #print(len(newRow))
-            i = i + 1
-        i = 0
-        promote.append(newRow)
-        promote = list(filter(bool, promote))
-        #print(promote)
-        #print(len(promote))
-        for element in promote:
-            #your code to update forecast here
-            if i != len(promote):
-                print("\nApproving Task Code:", taskCode, '\n')
-                print(', '.join(element), '\n\n')
-                i += 1
-                print("Group",i,"of", len(promote), '\n')
-                
-                #click Job Search Field
-                click('C:/projects/images/jobsearchfield.png', "Job Search Field")
+                block = block + row
+            rowIndex += 1
 
-                #type list of jobs to search for
-                pyautogui.typewrite(', '.join(element))
-
-                #click Search Button
-                click('C:/projects/images/searchButton.png', "Search Button")
-
-                #click Select All Jobs checkbox
-                click('C:/projects/images/selectalljobs.png', "Select All Jobs")
-
-                #click Next Button
-                click('C:/projects/images/next.png', "Next Button")
-
-                #click Filter Tasks
-                click('C:/projects/images/taskcodefilter.png', "Task Code Filter")
-                
-                #click Equal to field
-                click('C:/projects/images/isequalto.png', "Is Equal To")
-                
-                #Type taskcode into field
-                pyautogui.typewrite(taskCode)
-
-                #click filter button
-                click('C:/projects/images/filterbutton.png', "Filter Button")
-                                   
-                #click Promote to Approve select all checkbox
-                click('C:/projects/images/promoteselectall.png', "Promote Checkbox")
-                
-                #input("Press Enter to continue...")
-
-                #click submit button
-                click('C:/projects/images/submitbutton.png', "Submit button")
-                
-                #click Ok button
-                click('C:/projects/images/okbutton.png', "OK button")
-                
-                #click return button
-                click('C:/projects/images/returntohome.png', "Back Button")
-                          
+        blockList.append(block)
+        blockList = list(filter(bool, blockList))
     infile.close()
+    return blockList
+
+
+def start(taskCode, path):
+    allBlocks = parseBlocks(path)
+
+    for block in allBlocks:
+        if allBlocks.index(block) + 1 != len(allBlocks):
+            print("\nApproving Task Code:", taskCode, '\n')
+            print(', '.join(block), '\n\n')
+            print("Group", allBlocks.index(block) + 1,
+                  "of", len(allBlocks), '\n')
+
+            # click Job Search Field
+            click('C:/projects/images/jobsearchfield.png', "Job Search Field")
+
+            # type list of jobs to search for
+            pyautogui.typewrite(', '.join(block))
+
+            # click Search Button
+            click('C:/projects/images/searchButton.png', "Search Button")
+
+            # click Select All Jobs checkbox
+            click('C:/projects/images/selectalljobs.png', "Select All Jobs")
+
+            # click Next Button
+            click('C:/projects/images/next.png', "Next Button")
+
+            # click Filter Tasks
+            click('C:/projects/images/taskcodefilter.png', "Task Code Filter")
+
+            # click Equal to field
+            click('C:/projects/images/isequalto.png', "Is Equal To")
+
+            # Type taskcode into field
+            pyautogui.typewrite(taskCode)
+
+            # click filter button
+            click('C:/projects/images/filterbutton.png', "Filter Button")
+
+            # click Promote to Approve select all checkbox
+            click('C:/projects/images/promoteselectall.png',
+                  "Promote Checkbox")
+
+            #input("Press Enter to continue...")
+
+            # click submit button
+            click('C:/projects/images/submitbutton.png', "Submit button")
+
+            # click Ok button
+            click('C:/projects/images/okbutton.png', "OK button")
+
+            # click return button
+            click('C:/projects/images/returntohome.png', "Back Button")
+
 
 if __name__ == '__main__':
-    start("RE020")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    start("RE020", "C:/pathtocsv")
