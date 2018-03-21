5514cd93fed4770165aa2a1e0843704704683d77
Mark Halls <firesetterdev@gmail.com>
2018-03-20 17:18:52 -0700

    Merge functionality into approveByTask.py

--- projects/dailyapprovals.py
+++ projects/dailyapprovals.py
@@ -31,140 +31,54 @@ coorSearchButton = 0
 coorFilterButton = 0
 coorPromoteCheckbox = 0
 
-def jobSearchField():
+
+def click(fullPathToImage, name):
     try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/jobsearchfield.png')))
+        pyautogui.click(pyautogui.center(
+            pyautogui.locateOnScreen(fullPathToImage)))
     except:
-        print("Job Search Field not found, trying again.")
+        print(name, " not found, trying again")
+        click(fullPathToImage, name)
+
+
+def parseBlocks(path):
+    blockList = []
+    with open(path, mode='r') as infile:
+        reader = csv.reader(infile)
+        block = []
+        for row in reader:
+            block = block.append(row)
+            if len(block) >= 40:
+                blockList.append(block)
+                block = []
+        if len(block) > 0:
+            blockList.append(block)
+    infile.close()
+    return blockList
+
+
+for element in promote:
+    if not i == len(promote):
+        print("\nPromoting to Approve \nUsing 'Is Not Contained In' \nCI025, CI030, CI031, CI032, CI060, RE020, CI037, CI220\n")
+
+        i += 1
+        print("Group", i, " of ", len(promote), '\n')
+        print(', '.join(element), '\n\n')
+
         jobSearchField()
-    
-def jobSearchButton():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/searchButton.png')))
-    except:
-        print("Search Button not found, trying again.")
+        pyautogui.typewrite(', '.join(element))
         jobSearchButton()
-
-def selectAllJobs():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/selectalljobs.png')))
-    except:
-        print("Select All Jobs greyed out, trying again.")
         selectAllJobs()
-
-def clickNext():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/next.png')))
-    except:
-        print("Next Button greyed out, trying again.")
         clickNext()
-
-def taskFilter():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/taskcodefilter.png')))
-    except:
-        print("Could not find Task Code Filter, trying again.")
-        taskFilter()        
-
-def filterDropDown():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/filterdropdown.png')))
-    except:
-        print("Could not find Filter Dropdown, trying again.")
-        filterDropDown()        
-
-def selectIsNotContainedIn():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/selectisnotcontainedin.png')))
-    except:
-        print("Could not find Is Not Contained In, trying again.")
-        selectIsNotContainedIn()    
-
-def isNotContainedInFilter():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/isnotcontainedinfilter.png')))
-    except:
-        print("Could not find Is Not Contained In filter field, trying again.")
-        isNotContainedInFilter()        
-        
-def filterButton():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/filterbutton.png')))
-    except:
-        print("Filter Button not found, trying again.")
+        taskFilter()
+        filterDropDown()
+        selectIsNotContainedIn()
+        isNotContainedInFilter()
+        pyautogui.typewrite(
+            "CI025, CI030, CI031, CI032, CI060, RE020, CI037, CI220")
         filterButton()
-
-def promoteCheckbox():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/promoteselectall.png')))
-    except:
-        print("Promote Checkbox not found, trying again.")
-        promoteCheckbox()        
-        
-def submit():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/submitbutton.png')))
-    except:
-        print("Could not find Submit button, trying again.")
+        promoteCheckbox()
         submit()
-
-def okButton():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/okbutton.png')))
-    except:
-        print("Could not find OK button, trying again.")
         okButton()
-        
-def returnToSearch():
-    try:
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('C:/projects/images/returntohome.png')))
-    except:
-        print("Could not find Back Button, trying again")
         returnToSearch()
-        
-        
-with open("c:/projects/csv/approvals.csv") as f:
-    reader = csv.reader(f)
-    i = 0
-    promote = []
-    newRow = []
-    for row in reader:
-        if i % 75 == 0:
-            promote.append(newRow)
-            newRow = row
-            #print("1 newlistpromote") 
-        else:
-            newRow = newRow + row
-            #print(len(newRow))
-        i = i + 1
-    i = 0
-    promote.append(newRow)
-    promote = list(filter(bool, promote))
-    #print(promote)
-    #print(len(promote))
-    for element in promote:
-        if not i == len(promote):
-            print("\nPromoting to Approve \nUsing 'Is Not Contained In' \nCI025, CI030, CI031, CI032, CI060, RE020, CI037, CI220\n")
-            
-            i += 1
-            print("Group",i," of ", len(promote), '\n')
-            print(', '.join(element), '\n\n')
-            
-            jobSearchField()
-            pyautogui.typewrite(', '.join(element))
-            jobSearchButton()
-            selectAllJobs()
-            clickNext()
-            taskFilter()
-            filterDropDown()
-            selectIsNotContainedIn()
-            isNotContainedInFilter()
-            pyautogui.typewrite("CI025, CI030, CI031, CI032, CI060, RE020, CI037, CI220")
-            filterButton()
-            promoteCheckbox()
-            submit()
-            okButton()
-            returnToSearch()
-    f.close()
-    
-    
+f.close()
